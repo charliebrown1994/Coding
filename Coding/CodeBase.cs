9c108a0eb95491bc3aeeee83898222334f3160d1
charliebrown1994 <leesungyul@outlook.com>
2025-09-14 02:42:58 +0900

    Feat : 2부 C# 기초
    - Add New Class

---
+++ Coding/CodeBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding
+{
+    internal class CodeBase
+    {
+        // 변수 만들기
+        // 메모리를 잠시 보과해 두고 사용할 수 있는 임시 저장공간 일반적으로 값을 담아 놓는 박스라고 생각한다.
+        // 데이터 형식을 자료형이라고 하는데 이건 박스에 테그를 부착해서 해당 박스는 테그에 적인 값만 담을 수 있도록 하는 것과 같다.
+        int number; // int형 변수 number 선언
+        // 대표적인 정수형
+        // int, long, float, double, string, char, bool등이 있다.
+
+    }
+}
